141f57d3b3312b2a145145f88aac651b2bcdcd01
misarb <lahssane12@gmail.com>
2023-03-22 23:07:20 +0000

    add new prblm

--- call_of_mycode/src/Roman_to_Integer.rs
+++ call_of_mycode/src/Roman_to_Integer.rs
@@ -7,6 +7,7 @@ impl SolutionRomanIntegers {
         let pairs = vec![("I",1),("V",5),("X",10),("L",50),("C",100),("D",500),("M",1000)];
         for (index,item) in pairs.iter().enumerate() {
             match &self => {
+                
 
             }
 
